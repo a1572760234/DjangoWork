430d2f1f7f6a928cc3497db9277159788fde73bb
刘翔宇 <a1572760234@126.com>
2020-09-27 03:59:27 +0800

    设置启用session&设置session存储于数据库

--- django04_MVT/django04_MVT/settings.py
+++ django04_MVT/django04_MVT/settings.py
@@ -111,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -126,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SESSION_ENGINE='django.contrib.sessions.backends.db'
--- django04_MVT/myapp/views.py
+++ django04_MVT/myapp/views.py
@@ -12,7 +12,10 @@ def userRequest(request,request1_id,request2_id):
 def cookie(request):
     #设置cookie函数
     response = HttpResponse('request ok')
-    response.set_cookie('testcookie','cookievalue')
-    response.set_cookie('testcookie2', 'cookievalue2')
+    response.set_cookie('testcookie','cookievalue',max_age=3600)
+    response.set_cookie('testcookie2', 'cookievalue2',max_age=3600)
     print(request.COOKIES.get('testcookie'),request.COOKIES.get('testcookie2'))
+    #删除cookie
+    # response.delete_cookie('testcookie')
+    # response.delete_cookie('testcookie2')
     return response
